084f86439d00c65bdd43fb4016fe440526e41c3a
Admin <admin@Alis-MacBook-Pro.local>
2018-01-15 15:14:09 +0000

    Removed Solar Compiler document and moved contents into directory.py. Added new classes 'Help' and 'SolarHelper' to contain all functions that were in solarsystem.py. Changed name of some functions to make them make more sense.

--- directory.py
+++ directory.py
@@ -1,4 +1,5 @@
 
+
 class Planet():
     """This class defines the different variables to be initiated one every planet."""
     def __init__(self, planet_name, planet_mass, planet_distanceFromSun, planet_funFact, planet_diameter, number_of_moons, orbitalPeriod):
@@ -11,16 +12,14 @@ class Planet():
         self.orbitalPeriod = orbitalPeriod
 
 class Jovian(Planet):
-    def __init__(self, planet_name, planet_mass, planet_distanceFromSun, planet_funFact, planet_diameter, number_of_moons, orbitalPeriod, composition):
+    def __init__(self, planet_name, planet_mass, planet_distanceFromSun, planet_funFact, planet_diameter, number_of_moons, orbitalPeriod):
         Planet.__init__(self, planet_name, planet_mass, planet_distanceFromSun, planet_funFact, planet_diameter, number_of_moons, orbitalPeriod)
-        self.composition = composition
+        self.composition = 'gas'
 
 class Terrestrial(Planet):
-    def __init__(self, planet_name, planet_mass, planet_distanceFromSun, planet_funFact, planet_diameter, number_of_moons, orbitalPeriod, composition):
+    def __init__(self, planet_name, planet_mass, planet_distanceFromSun, planet_funFact, planet_diameter, number_of_moons, orbitalPeriod):
         Planet.__init__(self, planet_name, planet_mass, planet_distanceFromSun, planet_funFact, planet_diameter, number_of_moons, orbitalPeriod)
-        self.composition = composition
-
-
+        self.composition = 'rock'
 
 class Star():
     """This class defines the different variables to be initiated one every planet."""
@@ -32,31 +31,49 @@ class Star():
         self.funFact = star_fun_fact
         self.surfaceTemp = star_surface_temp
         self.coreTemp = star_core_temp
-'''
-class Composition(Planet):
-    """This class takes the composition of a planet, defined in the class 'Planet', and determines whether it is composed mainly of rock or gas."""
-    def __init__(self, planet_name, planet_mass, planet_composition, planet_distanceFromSun, planet_funFact, planet_diameter, number_of_moons, orbitalPeriod):
-        def getComposition(self):
-            if planet_composition == "terrestrial":
-                return "This planet is a rocky body."
-            elif planet_composition == "jovian":
-                if planet_name == "Jupiter" or planet_name == "Saturn":
-                    return "This planet is a gaseous body."
-                elif planet_name == "Neptune" or planet_name == "Uranus":
-                    return "This planet is an icy and gaseous body."
-'''
-'''
-class solarHelper():
+
+
+class Help():
+    def __init__(self):
+        pass
+    
+    def assist(self):
+        return """To order the planets alphabetically:    solarsystem.py list \n
+    To order the planets by mass (highest to lowest):   solarsystem.py list -orderby mass \n
+    To order the planets by diameter (highest to lowest):   solarsystem.py list -orderby diameter \n
+    To print details and an interesting fact about an individual planet: solarsystem.py planet <insert planet name>"""
+
+
+class SolarHelper():
     """This class contains all functions that are called within 'solarsystem.py'."""
-    def assist():
-        return """To order the planets alphabetically:    solarsystem.py list \nTo order the planets by mass (highest to lowest):   solarsystem.py list -orderby mass \nTo order the planets by diameter:   solarsystem.py list -orderby diameter"""
-    def alphabetically():
-        for i in planets:
-            newPlanets.append(i.name)
-        return sorted(newPlanets)
-    def factAndComp():
-        usrInp = sys.argv[2].lower()
-        for planet in planets:
+    def __init__(self):
+        pass
+    
+    mercury = Terrestrial("Mercury", 3.285 * (10**23), 57910000, "Mercury, despite being the closest planet to the sun by quite a margin, is not actually the planet with the highest surface temperature, as it has no atmosphere to keep the heat in. That award goes to Venus, which is 467 C, 40 C hotter than Mercury.", 4879, 0, 88)
+    venus = Terrestrial("Venus", 4.867 * (10**24), 108200000, "Due to the fact that Venus's rotation is retrograde (the opposite direction to which it orbits the sun) and it rotates very slowly, a day on the planet lasts longer than a it's year. It takes 225 Earth-days to orbit the sun but 243 Earth-days to complete a single rotation.", 12104, 0, 225)
+    earth = Terrestrial("Earth", 5.972 * (10**24), 149600000, "Earth... is here!", 12742, 1, 365.25)
+    mars = Terrestrial("Mars", 6.39 * (10**23), 227900000, "some information will be added", 6779, 2, 687)
+    jupiter = Jovian("Jupiter", 1.898 * (10**27), 778500000, "Jupiter's red spot is a storm that is so large that it could fit the Earth inside of it, and it has been raging for at least 350 years. However, it is shrinking.", 69911, 69, 4332.59)
+    saturn = Jovian("Saturn", 5.683 * (10**26), 1429000000, "Saturn is the only planet in the soslar system that is less dense (30% less) than water. That means that if you were able to find a large enough ocean, it would float!", 58232, 62, 10759.22)
+    uranus = Jovian("Uranus", 3.285 * (10**26), 2871000000, "some information will be added", 50274, 27, 30685)
+    neptune = Jovian("Neptune", 1.024 * (10**26), 4498000000, "some information will be added", 49244, 14, 60182)
+
+    planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
+    planetsMass = []
+    planetsDiameter = []
+    newPlanets = []
+
+    def alphabetically(self):
+        print "A list of the planets in the solar system, sorted alphabetically: "
+        print "---"
+        for i in self.planets:
+            self.newPlanets.append(i.name)
+        return sorted(self.newPlanets)
+
+
+    def factAndComp(self, planetName):
+        usrInp = planetName.lower()
+        for planet in self.planets:
             if planet.name.lower() == usrInp:
                 print planet.name + ":"
                 print "---"
@@ -66,24 +83,33 @@ class solarHelper():
                 print "---"
                 print "The diameter of " + planet.name + " is " + str(planet.diameter / 1000.0) + " thousand km."
                 print "---"
-                print "The distance of " + planet.name + " from the Sun is " + str(planet.distanceFromSun / 1000000) + " million km."
+                return "The distance of " + planet.name + " from the Sun is " + str(planet.distanceFromSun / 1000000) + " million km."
+        return "That is not a valid planet"
 
-    def massSort():
-        for i in planets:
-            newPlanets.append(i.mass)
-        newPlanetsMass = sorted(newPlanets, reverse=True)
+
+    def massSort(self):
+        print "A list of the planets in the solar system, sorted by mass, from largest to smallest: "
+        print "---"
+        for i in self.planets:
+            self.newPlanets.append(i.mass)
+        newPlanetsMass = sorted(self.newPlanets, reverse=True)
         for i in newPlanetsMass:
-            for j in planets:
+            for j in self.planets:
                 if j.mass == i:
-                    planetsMass.append(j.name)
-        return planetsMass
-    def diaSort():
-        for i in planets:
-            newPlanets.append(i.diameter)
-        newPlanetsMass = sorted(newPlanets, reverse=True)
+                    self.planetsMass.append(j.name)
+        return self.planetsMass
+
+
+    def PrintPlanets(self):
+        print "A list of the planets in the solar system, sorted by their diameter, from largest to smallest: "
+        print "---"
+        for i in self.planets:
+            self.newPlanets.append(i.diameter)
+        newPlanetsMass = sorted(self.newPlanets, reverse=True)
         for i in newPlanetsMass:
-            for j in planets:
+            for j in self.planets:
                 if j.diameter == i:
-                    planetsDiameter.append(j.name)
-        return planetsDiameter
-'''
+                    self.planetsDiameter.append(j.name)
+        return self.planetsDiameter
+
+
--- solarsystem.py
+++ solarsystem.py
@@ -1,113 +1,35 @@
 import sys
-from solarCompiler import *
+
 from directory import *
 
-def assist():
-    return """To order the planets alphabetically:    solarsystem.py list \nTo order the planets by mass (highest to lowest):   solarsystem.py list -orderby mass \nTo order the planets by diameter (highest to lowest):   solarsystem.py list -orderby diameter \nTo print details and an interesting fact about an individual planet: solarsystem.py planet <insert planet name>"""
-def alphabetically():
-    print "A list of the planets in the solar system, sorted alphabetically: "
-    print "---"
-    for i in planets:
-        newPlanets.append(i.name)
-    return sorted(newPlanets)
-def factAndComp():
-    usrInp = sys.argv[2].lower()
-    for planet in planets:
-        if planet.name.lower() == usrInp:
-            print planet.name + ":"
-            print "---"
-            print "Fun fact: " + planet.funFact
-            print "---"
-            print "The composition of this planet is primarily " + planet.composition
-            print "---"
-            print "The diameter of " + planet.name + " is " + str(planet.diameter / 1000.0) + " thousand km."
-            print "---"
-            print "The distance of " + planet.name + " from the Sun is " + str(planet.distanceFromSun / 1000000) + " million km."
-    return "That is not a valid planet"
 
-def massSort():
-    print "A list of the planets in the solar system, sorted by mass, from largest to smallest: "
-    print "---"
-    for i in planets:
-        newPlanets.append(i.mass)
-    newPlanetsMass = sorted(newPlanets, reverse=True)
-    for i in newPlanetsMass:
-        for j in planets:
-            if j.mass == i:
-                planetsMass.append(j.name)
-    return planetsMass
-def diaSort():
-    print "A list of the planets in the solar system, sorted by their diameter, from largest to smallest: "
-    print "---"
-    for i in planets:
-        newPlanets.append(i.diameter)
-    newPlanetsMass = sorted(newPlanets, reverse=True)
-    for i in newPlanetsMass:
-        for j in planets:
-            if j.diameter == i:
-                planetsDiameter.append(j.name)
-    return planetsDiameter
 if len(sys.argv) == 1:
-    print assist()
+    print Help().assist()
 
 elif len(sys.argv) == 2:
     if sys.argv[1] == "--help":
-        print assist()
+        print Help().assist()
     elif sys.argv[1] == "list":
-        print alphabetically()
+        print SolarHelper().alphabetically()
     else:
-        print assist()
+        print Help().assist()
 elif len(sys.argv) == 3:
     if sys.argv[1] == "planet":
-        print factAndComp()
+        print SolarHelper().factAndComp(sys.argv[2])
     else:
-        print assist()
+        print Help().assist()
 elif len(sys.argv) == 4:
     if sys.argv[1] == "list":
         if sys.argv[2] == "-orderby":
             if sys.argv[3] == "mass":
-                print massSort()
+                print SolarHelper().massSort()
             elif sys.argv[3] == "diameter":
-                print diaSort()
+                print SolarHelper().diaSort()
             else:
-                print assist()
+                print Help().assist()
         else:
-            print assist()
+            print Help().assist()
     else:
-        print assist()
+        print Help().assist()
 else:
-    print assist()
-'''
-if len(sys.argv) == 1:
-    print 'help'
-elif len(sys.argv) == 2:
-    if sys.argv[1] == '--help':
-        print 'help'
-    elif sys.argv[1] == 'list':
-        print 'list'
-    else:
-        print 'help'
-elif len(sys.argv) == 3:
-    if sys.argv[1] == 'planet':
-        if sys.argv[2] == 'planet':
-            print 'Interesting info on <planet>, including its diameter, mass, distance from sun and composition.'
-        else:
-            print 'help here'
-    else:
-        print 'help'
-elif len(sys.argv) == 4:
-    if sys.argv[1] == 'list':
-        if sys.argv[2] == '-orderby':
-            if sys.argv[3] == 'mass':
-                print 'List of Planets in order of mass, largest to smallest.'
-            elif sys.argv[3] == 'diameter':
-                print 'List of Planets in order of diameter, largest to smallest.'
-            else:
-                print 'help here'
-        else:
-            print 'help here'
-    else:
-        print 'help here'
-else:
-    print 'help here'
-'''
+    print Help().assist()
